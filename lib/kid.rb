f1ca08ed79ae72f1baeadcd278bfd780067fae3e
github username <github email address>
2019-01-17 18:16:30 -0500

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance #allow our classes to use all of the methods of the included module as instance methods.
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
